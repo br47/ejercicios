cab7f5fb87a4f3f022030869e766ad943cef0909
br47 <briancalabuig@gmail.com>
2022-02-02 08:44:37 +0100

    Ejercicio de transacciones resuelto

--- transacciones_sql/backend/savings_backend.py
+++ transacciones_sql/backend/savings_backend.py
@@ -20,13 +20,14 @@ def set_credit(conn, userid, amount):
     conn.execute("update savings set amount = ? where userid = ?", (amount, userid))
 
 def transfer(conn, user_a, user_b, transfer_amount):
-    user_a_credit = get_credit(conn, user_a)
-    assert user_a_credit >= transfer_amount
+    with conn:
+        user_a_credit = get_credit(conn, user_a)
+        assert user_a_credit >= transfer_amount
 
-    user_b_credit = get_credit(conn, user_b)
+        user_b_credit = get_credit(conn, user_b)
 
-    new_user_a_credit = user_a_credit - transfer_amount
-    set_credit(conn, user_a, new_user_a_credit)
+        new_user_a_credit = user_a_credit - transfer_amount
+        set_credit(conn, user_a, new_user_a_credit)
 
-    new_user_b_credit = user_b_credit + transfer_amount
-    set_credit(conn, user_b, new_user_b_credit)
+        new_user_b_credit = user_b_credit + transfer_amount
+        set_credit(conn, user_b, new_user_b_credit)
